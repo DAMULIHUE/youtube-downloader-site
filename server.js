73a87032003bd7ecbb0a617eaf7ed3fbe527d3ec
DAMULIHUE <lihue@outlook.com.br>
2025-06-14 20:44:29 +0100

    new features: resolution | no support to mp3 files

--- server.js
+++ server.js
@@ -5,19 +5,20 @@ const app = express();
 
 //dowload video (test purpose)
 
-async function downloadServidor(url){
+async function downloadServidor(url, format, resolution){
 	
 	fs.unlink('./public/video.mp4', (err) => console.log(err || "video.mp4 foi deletado")) 
 	const video = await ytDl(url , {
-		format: "mp4",
+		format: format,
+		formatSort: `res:${resolution}`,
 		paths: "./public",
-		o: "video.mp4"
+		o: `video.${format}`
 	}).then(output => { 
-		// retorna o getFilename (ver doc da lib)
+		// retorna o 'o' (o é 'output') (ver doc da lib)
 		return output;
 	});
 	
-	// retorna o path do video
+	// retorna o path do video ('o')
 	return video;
 }
 
@@ -28,10 +29,10 @@ app.use(express.json());
 
 app.post("/video", async (req, res, next) => {
 	
-	const { url } = req.body;
-	const video = await downloadServidor(url);
+	const { url, format, resolution } = req.body;
+	const video = await downloadServidor(url, format, resolution);
 	console.log(video);
-	res.sendFile(`video.mp4`, { root: "./public" });
+	res.sendFile(`video.${format}`, { root: "./public" });
 })
 
 app.listen(6969, (err) => {
